12cf788e9a2c32bbca0dff7b0314a044775930ad
Alexander Rogachev <alexanderr@mac-mini-admin.local>
2016-04-17 23:31:22 +0300

    Removed AnswerView.

---
+++ EnglishTraining/EnglishTraining/AnswerView.h
@@ -0,0 +1,13 @@
+//
+//  AnswerView.h
+//  EnglishTraining
+//
+//  Created by Alexander Rogachev on 4/17/16.
+//  Copyright © 2016 Alexander Rogachev. All rights reserved.
+//
+
+#import "SceneBehaviour.h"
+
+@interface AnswerView : SceneBehaviour
+
+@end
